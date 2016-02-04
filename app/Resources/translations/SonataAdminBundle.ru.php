b18bc99dfd6b612b05a1d937bb91cadee117737e
hitakiri <evander2010@yandex.ru>
2016-02-04 16:59:30 +0300

    Часть перевода

---
+++ app/Resources/translations/SonataAdminBundle.ru.php
@@ -0,0 +1,6 @@
+<?php
+return [
+	'library_main' => 'Основная',
+	'library_secondary' => 'Дополнительная',
+	'library_links' => 'Ссылки',
+];
---
+++ app/Resources/translations/validators.ru.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	'This value is too short. It should have {{ limit }} characters or more' => 'Сообщение должно содержать по крайней мере {{ limit }} знака.',
+	'notspam' => 'Ваш комментарий был идентифицирован как спам..',
+	'This value is already used.' => 'Уже используется.',
+	'This slug is already in use.' => 'Такой идентификатор уже используется.',
+	'This form should not contain extra fields.' => 'Эта форма не должна содержать дополнительные поля.',
+	'The CSRF token is invalid. Please try to resubmit the form.' => 'Маркер CSRF является недействительным. Пожалуйста, попробуйте отправить форму заново.',
+];
