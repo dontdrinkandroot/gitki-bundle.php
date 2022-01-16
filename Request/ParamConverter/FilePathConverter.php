d1ce077293192feb9edc1e27143b3ca1c025b460
Philip Washington Sorst <philip@sorst.net>
2022-01-16 18:26:20 +0100

    Adding PathConverters

---
+++ Request/ParamConverter/FilePathConverter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Dontdrinkandroot\GitkiBundle\Request\ParamConverter;
+
+use Dontdrinkandroot\Common\Asserted;
+use Dontdrinkandroot\Path\FilePath;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FilePathConverter implements ParamConverterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(Request $request, ParamConverter $configuration): bool
+    {
+        $param = $configuration->getName();
+
+        if (!$request->attributes->has($param)) {
+            return false;
+        }
+
+        $request->attributes->set($param, FilePath::parse(Asserted::string($request->attributes->get($param))));
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(ParamConverter $configuration): bool
+    {
+        return $configuration->getClass() === FilePath::class;
+    }
+}
